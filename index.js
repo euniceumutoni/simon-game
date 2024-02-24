29f1c6d8d8504fec18b26c38a7507cda49ba11ee
Eunice Umutoni <136147297+euniceumutoni@users.noreply.github.com>
2024-02-23 20:31:01 -0600

    index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var level = 0;
 
 $(document).keypress(function() {
   if (!started) {
-    $("#level-title").text("Level " + level);
+    $("#level-title").text("Level " + level); 
     nextSequence();
     started = true;
   }
